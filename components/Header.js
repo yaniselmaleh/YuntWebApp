cd0adbf431fe38180293464d7409057e1c7db185
Votre nom <contact@yaniselmaleh.fr>
2019-06-18 21:39:20 +0200

    Update Yunt.io

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,6 @@ const Header = () => (
   <header>
     <Link href="/">
       <a title="home">Home</a>
-      <img src="/static/image.png" alt="image" />
     </Link>
     <Link href="/about">
       <a title="about">About</a>
