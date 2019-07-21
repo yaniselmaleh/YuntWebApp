55de99cf330b465fb9f7dec7fb840aae899c9594
yanis elmaleh <yaniselmaleh@MacBook-Pro-de-yanis.local>
2019-07-21 23:12:57 +0200

    Reset

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,7 @@ export default class MyDocument extends Document {
         <meta charSet="utf-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, maximum-scale=1.0, minimal-ui"/>
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,800|Playfair+Display:400,700" rel="stylesheet"/>
-        <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:400i&display=swap" rel="stylesheet">
+        {/* <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:400i&display=swap" rel="stylesheet"> */}
         <body data-lang="fr" data-countrycode="fr">
           <main>
             <Main />
