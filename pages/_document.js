f9f4fb28c859eba19686e21f5ba3e8dcdfcdc26b
yaniselmaleh <44808736+yaniselmaleh@users.noreply.github.com>
2019-07-22 14:44:32 +0200

    Update Font.js

--- pages/_document.js
+++ pages/_document.js
@@ -8,8 +8,8 @@ export default class MyDocument extends Document {
         <title>Yunt</title>
         <meta charSet="utf-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, maximum-scale=1.0, minimal-ui"/>
-        {/* <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:400,400i|Open+Sans:400,600,800|Playfair+Display:400,700&display=swap" rel="stylesheet">  */}
-        <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,800|Playfair+Display:400,700" rel="stylesheet"/>
+        {/* <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:300,400,400i|Open+Sans:400,600,800|Playfair+Display:400,700&display=swap" rel="stylesheet">  */}
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,800|Playfair+Display:400,700" rel="stylesheet"/>
         {/* <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:400i" rel="stylesheet"> */}
         <body data-lang="fr" data-countrycode="fr">
           <main>
