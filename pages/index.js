e320d6f680a74c7011fb3ff08285149fd26cb5a4
yaniselmaleh <44808736+yaniselmaleh@users.noreply.github.com>
2019-07-23 00:24:21 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
 import Home from '../components/Home';
 import Main from '../components/Main';
-// import Bot from '../components/Bot';
 
 export default function Index() {
   return (
     <React.Fragment>
-    // <Bot/>
       <Home/>
       <Main/>
     </React.Fragment>
