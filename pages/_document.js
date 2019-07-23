970922a2aed7d937942ba0c7037a01fab29f4dba
yaniselmaleh <44808736+yaniselmaleh@users.noreply.github.com>
2019-07-23 08:35:33 +0200

    Update document.js

--- pages/_document.js
+++ pages/_document.js
@@ -15,7 +15,50 @@ export default class MyDocument extends Document {
           <main>
             <Main />
             <NextScript />
+      <div
+         class="fb-customerchat"
+         page_id="342493183076214"
+         ref="live_chat"
+         theme_color="optional hex color code"
+         logged_in_greeting="optional greeting message for logged in users"
+         logged_out_greeting="optional greeting message for logged out users">
+     </div>
           </main>
+      
+      
+
+     <script>
+         var div = document.createElement('div');
+         div.className = 'fb-customerchat';
+         div.setAttribute('page_id', '342493183076214');
+         div.setAttribute('ref', 'b64:bGl2ZV9jaGF0');
+         document.body.appendChild(div);
+         window.fbMessengerPlugins = window.fbMessengerPlugins || {
+           init: function () {
+             FB.init({
+               appId            : '1678638095724206',
+               autoLogAppEvents : true,
+               xfbml            : true,
+               version          : 'v3.0'
+             });
+           }, callable: []
+         };
+         window.fbAsyncInit = window.fbAsyncInit || function () {
+           window.fbMessengerPlugins.callable.forEach(function (item) { item(); });
+           window.fbMessengerPlugins.init();
+         };
+         setTimeout(function () {
+           (function (d, s, id) {
+             var js, fjs = d.getElementsByTagName(s)[0];
+             if (d.getElementById(id)) { return; }
+             js = d.createElement(s);
+             js.id = id;
+             js.src = "//connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+             fjs.parentNode.insertBefore(js, fjs);
+           }(document, 'script', 'facebook-jssdk'));
+         }, 0);
+       </script>
+
         </body>
       </html>
     );
